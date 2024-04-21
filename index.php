a97f339e692587e3c33092c2e1c093ff07eb2e43
Ignacio Donoso <ignacio.donoso.s@gmail.com>
2024-04-21 18:22:00 -0400

    FEATURE[JOSEIGNACIO] mision and vision updated

--- index.php
+++ index.php
@@ -20,16 +20,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#home">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Nuestros Servicios</a>
+                <a class="nav-link" href="#services">Nuestros Servicios</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Contáctenos</a>
+                <a class="nav-link" href="#contact">Contáctenos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Nosotros</a>
+                <a class="nav-link" href="#aboutUs">Nosotros</a>
             </li>
             <li class="nav-item">
                 <button id="toggleMode" class="btn btn-link">Cambiar Modo</button>
@@ -39,7 +39,7 @@
 </nav>
 
 <!-- Home -->
-<div class="container mt-5">
+<div class="container mt-5" id="home">
     <h1>¡Bienvenidos a Mi Empresa!</h1>
     <p>Introducción a nuestra empresa...</p>
 </div>
@@ -47,28 +47,12 @@
 <!-- Nuestros Servicios -->
 <div class="container mt-5">
     <h2>Nuestros Servicios</h2>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Consultoría Digital</h5>
-                    <p class="card-text">Descripción de nuestros servicios de consultoría digital.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Soluciones Multiexperiencia</h5>
-                    <p class="card-text">Descripción de nuestras soluciones multiexperiencia.</p>
-                </div>
-            </div>
-        </div>
+    <div class="row" id="services">
     </div>
 </div>
 
 <!-- Contáctenos -->
-<div class="container mt-5">
+<div class="container mt-5" id="contact">
     <h2>Contáctenos</h2>
     <form>
         <div class="form-group">
@@ -82,15 +66,24 @@
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
 </div>
-
-<!-- Nosotros -->
-<div class="container mt-5">
-    <h2>Nosotros</h2>
-    <p>Misión y visión de nuestra empresa...</p>
-</div>
+<section class="py-3 py-md-5">
+    <div class="container">
+        <div class="row gy-3 gy-md-4 gy-lg-0 align-items-lg-center">
+            <div class="col-12 col-lg-6 col-xl-5">
+                <img class="img-fluid rounded" loading="lazy" src="./images/con-ingenio-solo-colores.png" alt="About 1">
+            </div>
+            <div class="col-12 col-lg-6 col-xl-7">
+                <div class="row justify-content-xl-center">
+                    <div class="col-12 col-xl-11" id="aboutUs">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- Agrega los enlaces a los archivos JavaScript de Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <!-- Script para alternar entre los modos de estilo -->
